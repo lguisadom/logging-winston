79fd4f642634c8504710f629ac7b4e2363ad05a3
Luis Guisado <lguisadom@gmail.com>
2022-12-27 19:59:07 -0500

    feat: update winston module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,51 @@
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import path from 'path';
+
+const { combine, timestamp, label, printf, splat, json, errors, prettyPrint } =
+  winston.format;
+
+const errorField = winston.format((info, opts) => {
+  if (info.stack) {
+    info.error = info.stack;
+    delete info.stack;
+  }
+  return info;
+});
 
 @Module({
+  imports: [
+    WinstonModule.forRootAsync({
+      useFactory: () => ({
+        level: 'info',
+        format: combine(
+          timestamp(),
+          splat(),
+          errors({ stack: true }),
+          errorField(),
+          prettyPrint(),
+        ),
+        defaultMeta: {
+          service: '{service-name}',
+          containerInfo: {
+            memoryLimitInMB: '{container size limit}',
+          },
+          env: process.env.ENV || 'dev',
+          serviceVersion: '{service-version}',
+        },
+        transports: [new winston.transports.Console()],
+        rejectionHandlers: [
+          new winston.transports.File({ filename: 'rejections.log' }),
+        ],
+        exitOnError: false,
+      }),
+      inject: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
